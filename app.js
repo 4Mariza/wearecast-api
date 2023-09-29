af03c54b4cf32810220bf7d51760f1725506bd14
MARIA LUIZA GOMES DE ALMEIDA <23111819@CFP127ALUNO.LOCAL>
2023-09-29 17:25:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const body = document.getElementById('body')
+function redirecionar () {
+    window.location.assign("./main.html")
+}
+
+
+
+body.addEventListener('click', redirecionar)

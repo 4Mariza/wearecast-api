81459766891ca56944181be2ce9a9ad63ba1ab22
MARIA LUIZA GOMES DE ALMEIDA <23111819@CFP127ALUNO.LOCAL>
2023-10-27 16:53:30 -0300

    responsivo - finalizar

--- js/api.js
+++ js/api.js
@@ -10,7 +10,7 @@ export const getForecastToday = async (local) => {
 }
 
 export const getForecastWeekly = async (local) => {
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${local}&days=7&aqi=yes&alerts=yes`
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${local}&days=7&aqi=yes&alerts=yes`
     const response = await fetch(url)
     const data = await response.json()
     return data
